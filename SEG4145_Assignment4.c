79351c9d333789c30282ef1765bb91eaa27418e9
Qwertyyupitsme <shasta_the_dude@rogers.com>
2014-03-17 23:18:12 -0400

    FINALLY added message sending logic.

--- SEG4145_Assignment4.c
+++ SEG4145_Assignment4.c
@@ -35,6 +35,12 @@
 #define PERFORM_CIRCLE_CCW_MESSAGE 4
 #define INCREASE_CIRCLE_RADIUS_MESSAGE 5
 
+// Message queue constants
+#define MOTOR_QUEUE_SIZE 1
+
+// Debug flag
+// #define DEBUG_MODE
+
 /*
 *********************************************************************************************************
 *                                           FUNCTION PROTOTYPES
@@ -46,6 +52,14 @@ void TaskStart(void *data);				/* Startup task							 */
 static void TaskStartCreateTasks(void);         /* Will be used to create all the child tasks          */
 void lcdTask(void*); /* Task 1 declaration */
 void Task2(void*); /* Task 2 declaration */
+<<<<<<< HEAD
+=======
+void keyboardControlTask(void*); /* keyboardControlTask declaration */
+void sendMessage(OS_EVENT* queue, OS_EVENT* sem, void* msg); /* sendMessage declaration */
+void* receiveMessage(OS_EVENT* queue, OS_EVENT* sem); /* receiveMessage declaration */
+void printQueueError(INT8U error); /* printQueueError declaration */
+void printSemaphoreError(INT8U error); /* printSemaphoreError declaration */
+>>>>>>> FINALLY added message sending logic.
 
 /*
 *********************************************************************************************************
@@ -56,9 +70,19 @@ void Task2(void*); /* Task 2 declaration */
 OS_STK TaskStartStk[TASK_STK_SIZE];			/* Start task's stack						 */
 OS_STK TaskStk[N_TASKS][TASK_STK_SIZE];         /* Stacks for other (child) tasks				 */
 INT8U TaskData[N_TASKS];				/* Parameters to pass to each task                     */
+<<<<<<< HEAD
 OS_EVENT* queue;
 char dummyArr[5];
 void* TaskPointers[N_TASKS] = { lcdTask, Task2 };	/* Function pointers to the tasks */
+=======
+
+// Motor queue variables
+OS_EVENT* motorQueue; /* The queue for sending messages to the motor */
+void* motorQueueData[MOTOR_QUEUE_SIZE]; /* The memory space for the motor queue */
+OS_EVENT* motorSem; /* The semaphore controlling access to the motor queue */
+
+void* TaskPointers[N_TASKS] = { Task1, Task2, keyboardControlTask };	/* Function pointers to the tasks */
+>>>>>>> FINALLY added message sending logic.
 
 /*
 *********************************************************************************************************
@@ -108,12 +132,13 @@ void TaskStart(void *pdata)
            ((FP32) OSVersion())/100, ((FP32)OSPortVersion())/100);
     printf("Press the Escape key to stop.\n\n");
 
+	// Motor queue initialization
+	motorQueue = OSQCreate(motorQueueData, MOTOR_QUEUE_SIZE);
+	motorSem = OSSemCreate(MOTOR_QUEUE_SIZE);
+	
     /*
      * Here we create all other tasks (threads)
      */
-	void* pointer = (void*)dummyArr;
-	queue = OSQCreate(&pointer, 5);
-	if (queue == 0) printf("LOL U SUK");
     TaskStartCreateTasks();
 
     INT8U circle_ccw = 0; //true if circle goes counter-clockwise, false if circle goes clockwise
@@ -197,9 +222,6 @@ static void TaskStartCreateTasks(void)
 
 void lcdTask(void *pdata)
 {
-    INT8U whoami = *(int*) pdata;
-    INT8U counter = whoami % 2;
-
     while (1) {
      char *msg = "aaaaa"; //TODO read from message queue
 	 if(msg != NULL) {
@@ -214,67 +236,8 @@ void lcdTask(void *pdata)
 
 void Task2(void *pdata)
 {
-	/*int i = 0;
-	char msg = 'a';
-	INT8U* err;*/
 	int radius = 2;
     while (1) {
-		/*if (i == 0) {
-			INT8U result = OSQPost(queue, (void*)&msg);
-			switch (result) {
-				case OS_NO_ERR:
-					printf("Message was deposited in the queue.\n");
-					break;
-				case OS_Q_FULL:
-					printf("No room in the queue.\n");
-					break;
-				case OS_ERR_EVENT_TYPE:
-					printf("pevent is not pointing to a message queue.\n");
-					break;
-				case OS_ERR_PEVENT_NULL:
-					printf("pevent is a NULL pointer.\n");
-					break;
-				case OS_ERR_POST_NULL_PTR:
-					printf("msg is a NULL pointer.\n");
-					break;
-				}
-			i++;
-		} else {
-			char rec[2];
-			printf("getting value\n");
-
-			char* returned = (char*)OSQPend(queue, 0, err);
-
-			if (returned != 0) {
-				printf("got value\n");
-				rec[0] = *returned;
-				rec[1] = '\0';
-				printf("set null terminator\n");
-				printf(rec);
-				printf("printed\n");
-			} else {
-				printf("Didn't get notang.\n");
-				INT8U error = *err;
-				switch (error){
-					case OS_NO_ERR:
-						printf("Message was received");
-						break;
-					case OS_TIMEOUT:
-						printf("Message was not received within the specified timeout.");
-						break;
-					case OS_ERR_EVENT_TYPE:
-						printf("pevent is not pointing to a message queue");
-						break;
-					case OS_ERR_PEVENT_NULL:
-						printf("pevent is a NULL pointer");
-						break;
-					case OS_ERR_PEND_ISR:
-						printf("This function was called from an ISR and uC/OS-II must suspend the task. To avoid this don't call this function from an ISR.");
-						break;
-				}
-			}
-			i--;
-		}*/
 		INT8U cmd = -1; //TODO read from message queue
 		if(cmd == MOVE_TILE_FORWARD_MESSAGE) {
 			printf("Move one tile forward\n");
@@ -316,4 +279,90 @@ void sendMessageThingy(OS_EVENT* queue, void* msg) { //LOLOL LEO REPLACE THIS WI
     }
     return err;
     */
+}
+
+// Sends the given message to the given message queue. Returns 1
+// if it succeeds, 0 otherwise.
+void sendMessage(OS_EVENT* queue, OS_EVENT* sem, void* msg) {
+	INT8U result = -1;
+	INT8U semErr;
+	OSSemPend(sem, 0, &semErr); // Assume that nothing can go wrong
+#ifdef DEBUG_MODE
+		printSemaphoreError(semErr);
+#endif
+	while (result != OS_NO_ERR) { // Keep trying until it sends
+		result = OSQPost(queue, msg);
+#ifdef DEBUG_MODE
+		printQueueError(result);
+#endif
+		OSTimeDly(10);
+	}
+	fflush(stdout);
+}
+
+// Attempts to receive a message from the given message
+// queue. It blocks until a message is available. If an
+// error occurs, it returns a null pointer.
+void* receiveMessage(OS_EVENT* queue, OS_EVENT* sem) {
+	INT8U err;
+	void* received = OSQPend(queue, 0, &err);
+	INT8U semErr = OSSemPost(sem);
+#ifdef DEBUG_MODE
+	printQueueError(err);
+	printSemaphoreError(semErr);
+#endif
+	return received;
+}
+
+// Used for debugging. Prints the error message corresponding to
+// the given error code in the context of a message queue.
+void printQueueError(INT8U error) {
+	switch (error){
+		case OS_NO_ERR:
+			printf("Message was deposited in the queue or message was received\n");
+			break;
+		case OS_TIMEOUT:
+			printf("Message was not received within the specified timeout\n");
+			break;
+		case OS_ERR_PEND_ISR:
+			printf("This function was called from an ISR and uC/OS-II must suspend the task. To avoid this don't call this function from an ISR\n");
+			break;
+		case OS_Q_FULL:
+			printf("No room in the queue\n");
+			break;
+		case OS_ERR_EVENT_TYPE:
+			printf("pevent is not pointing to a message queue\n");
+			break;
+		case OS_ERR_PEVENT_NULL:
+			printf("pevent is a NULL pointer\n");
+			break;
+		case OS_ERR_POST_NULL_PTR:
+			printf("msg is a NULL pointer\n");
+			break;
+	}
+}
+
+// Used for debugging. Prints the error message corresponding to
+// the given error code in the context of a semaphore.
+void printSemaphoreError(INT8U error) {
+	switch (error) {
+		case OS_NO_ERR:
+			printf("Semaphore is available or has been signaled or released\n");
+			break;
+		case OS_TIMEOUT:
+			printf("Semaphore was not obtained within the specified timeout\n");
+			break;
+		case OS_ERR_EVENT_TYPE:
+			printf("pevent is not pointing to a semaphore\n");
+			break;
+		case OS_ERR_PEVENT_NULL:
+			printf("pevent is a NULL pointer\n");
+			break;
+		case OS_ERR_PEND_ISR:
+			printf("This function was called from an ISR and uC/OS-II must suspend the task. To avoid this don't call this function from an ISR\n");
+			break;
+		case OS_SEM_OVF:
+			printf("Semaphore counts has overflowed (> 65535)\n");
+			break;
+	}
 }
